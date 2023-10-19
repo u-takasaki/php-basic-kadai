322caba053f827b961d09180aad92d89ded5c994
u-takasaki <146915455+u-takasaki@users.noreply.github.com>
2023-10-19 16:04:00 +0900

    exam of continuous data.

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <p>
+    <?php
+      $product_detail = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+      print_r($product_detail);
+    ?>
+  </p>
+</body>
+</html>
